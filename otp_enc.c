10cec31af45e686a0d7e126ccbf5dac4d7f26eb9
Megan Aldridge <m.aldridge@live.com>
2017-03-13 22:56:16 -0400

    *

--- otp_enc.c
+++ otp_enc.c
@@ -8,6 +8,9 @@
 #include <netdb.h> 
 
 void error(const char *msg) { perror(msg); exit(0); } // Error function used for reporting issues
+int validate(char * text, char * key) {
+	return 1;
+}
 
 int main(int argc, char *argv[])
 {
@@ -16,14 +19,15 @@ int main(int argc, char *argv[])
 	struct hostent* serverHostInfo;
 	char buffer[256];
     
-	if (argc < 3) { fprintf(stderr,"USAGE: %s hostname port\n", argv[0]); exit(0); } // Check usage & args
+	if (argc != 4) { fprintf(stderr,"USAGE: %s plaintext key port\n", argv[0]); exit(1); } // Check usage & args
 
 	// Set up the server address struct
 	memset((char*)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
-	portNumber = atoi(argv[2]); // Get the port number, convert to an integer from a string
+	portNumber = atoi(argv[3]); // Get the port number, convert to an integer from a string
 	serverAddress.sin_family = AF_INET; // Create a network-capable socket
 	serverAddress.sin_port = htons(portNumber); // Store the port number
-	serverHostInfo = gethostbyname(argv[1]); // Convert the machine name into a special form of address
+	serverHostInfo = gethostbyname("localhost"); // Convert the machine name into a special form of address
+
 	if (serverHostInfo == NULL) { fprintf(stderr, "CLIENT: ERROR, no such host\n"); exit(0); }
 	memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length); // Copy in the address
 
@@ -35,6 +39,13 @@ int main(int argc, char *argv[])
 	if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to address
 		error("CLIENT: ERROR connecting");
 
+	// Validate that plaintext length > keylength 
+	if (validate(argv[1], argv[2])) {
+		printf("validated\n");
+	}
+	else {
+		printf("not validated\n");
+	}
 	// Get input message from user
 	printf("CLIENT: Enter text to send to the server, and then hit enter: ");
 	memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer array
