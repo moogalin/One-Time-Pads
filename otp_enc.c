9ee99ea88f1fa1c4622db9119f6549ecd037a404
Megan Aldridge <m.aldridge@live.com>
2017-03-14 22:14:25 -0400

    *

--- otp_enc.c
+++ otp_enc.c
@@ -6,10 +6,88 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
+#include <sys/stat.h>
+#include <errno.h>
 
 void error(const char *msg) { perror(msg); exit(0); } // Error function used for reporting issues
-int validate(char * text, char * key) {
-	return 1;
+
+int validSize(char * fText, char * fKey) {
+
+	struct stat st;
+	off_t textSize, keySize;
+
+	if (stat(fText, &st) == 0) {
+		textSize = st.st_size;
+	//	printf("textSize: %jd\n", (intmax_t)textSize);
+	}
+
+	else {
+		fprintf(stderr, "size error on %s: %s\n", fText, strerror(errno));
+	}
+
+	if (stat(fKey, &st) == 0) {
+		keySize = st.st_size;
+	//	printf("keySize: %jd\n", (intmax_t)keySize);
+	}
+	
+	else {
+		fprintf(stderr, "size error on %s: %s\n", fKey, strerror(errno));
+	}
+
+	if (keySize >= textSize) 
+		return 1;
+
+	else
+		return 0;
+
+	
+}
+
+int createMessage(char * message, size_t length, char *argv[]) {
+
+	int returnVal, i, ascii;
+	FILE *fp;
+	char buff1[8000];
+	char buff2[8000];
+
+	/* Create the plain text string */
+	fp = fopen(argv[1], "r");
+	fgets(buff1, 8000, fp);
+//	printf("buff1: %s", buff1);
+	fclose(fp);
+
+	/* Create the key string */
+	fp = fopen(argv[2], "r");
+	fgets(buff2, 8000, fp);
+//	printf("buff2: %s", buff2);
+	fclose(fp);
+
+	/* Remove newline character from each string */
+	buff1[strcspn(buff1, "\n")] = '\0';
+	buff2[strcspn(buff2, "\n")] = '\0';
+
+	/* Check plaintext string for bad characters */
+	for (i = 0; i < strlen(buff1); i++) {
+		ascii = buff1[i];
+		if (ascii < 65) {
+			if (ascii != 32) {
+				return 2;
+			}
+		}
+		else if (ascii > 90) {
+			return 2;
+		}
+
+	}
+	/* Create the message composed of the two strings */
+	returnVal = snprintf(message, length, "enc$$%s&&%s##", buff1 , buff2);
+
+	/* SnPrintf was unsuccessful, return failure */	
+	if (returnVal < 0) {return 1;}
+
+	/* Sucess */
+	return 0;
+
 }
 
 int main(int argc, char *argv[])
@@ -17,52 +95,82 @@ int main(int argc, char *argv[])
 	int socketFD, portNumber, charsWritten, charsRead;
 	struct sockaddr_in serverAddress;
 	struct hostent* serverHostInfo;
-	char buffer[256];
-    
-	if (argc != 4) { fprintf(stderr,"USAGE: %s plaintext key port\n", argv[0]); exit(1); } // Check usage & args
+	char buffer[4096];
+ 	int returnVal;
 
-	// Set up the server address struct
-	memset((char*)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
-	portNumber = atoi(argv[3]); // Get the port number, convert to an integer from a string
+	/* Check usage and args */   
+	if (argc != 4) { fprintf(stderr,"USAGE: %s plaintext key port\n", argv[0]); exit(1); } 
+
+	/* Set up the server address struct */
+	memset((char*)&serverAddress, '\0', sizeof(serverAddress)); //clear 
+	portNumber = atoi(argv[3]); // Convert string port to int
 	serverAddress.sin_family = AF_INET; // Create a network-capable socket
 	serverAddress.sin_port = htons(portNumber); // Store the port number
-	serverHostInfo = gethostbyname("localhost"); // Convert the machine name into a special form of address
+	serverHostInfo = gethostbyname("localhost"); // Convert name to special address
 
-	if (serverHostInfo == NULL) { fprintf(stderr, "CLIENT: ERROR, no such host\n"); exit(0); }
-	memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length); // Copy in the address
+	/* Error getting host by name */
+	if (serverHostInfo == NULL) { fprintf(stderr, "CLIENT: ERROR, no such host\n"); exit(1); }
 
-	// Set up the socket
+	/* Copy in the address */
+	memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length);
+
+	/* Set up the socket */
 	socketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
 	if (socketFD < 0) error("CLIENT: ERROR opening socket");
 	
-	// Connect to server
+	/* Connect to server */
 	if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to address
 		error("CLIENT: ERROR connecting");
 
-	// Validate that plaintext length > keylength 
-	if (validate(argv[1], argv[2])) {
+	/* Validate that plaintext length > keylength */
+	if (validSize(argv[1], argv[2])) {
 		printf("validated\n");
 	}
 	else {
-		printf("not validated\n");
+		fprintf(stderr, "Error: Key file may not be shorter than text file\n");
+		return 1;
 	}
+
 	// Get input message from user
-	printf("CLIENT: Enter text to send to the server, and then hit enter: ");
+/*	printf("CLIENT: Enter text to send to the server, and then hit enter: ");
 	memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer array
 	fgets(buffer, sizeof(buffer) - 1, stdin); // Get input from the user, trunc to buffer - 1 chars, leaving \0
 	buffer[strcspn(buffer, "\n")] = '\0'; // Remove the trailing \n that fgets adds
+*/
 
-	// Send message to server
+	/* Create input message from files */
+	memset(buffer, '\0', sizeof(buffer));
+	returnVal = createMessage(buffer, sizeof(buffer), argv);
+	
+	if (returnVal != 0) {
+		if (returnVal == 1) {
+			fprintf(stderr, "createMessage error\n");
+		}	
+		else if (returnVal == 2) {
+			fprintf(stderr, "createMessage error: invalid characters\n");
+		}
+		return 1;
+	}
+/*	else {
+		printf("buffer: %s\n", buffer);
+	}
+*/
+	/* Send message to server */
 	charsWritten = send(socketFD, buffer, strlen(buffer), 0); // Write to the server
 	if (charsWritten < 0) error("CLIENT: ERROR writing to socket");
 	if (charsWritten < strlen(buffer)) printf("CLIENT: WARNING: Not all data written to socket!\n");
 
-	// Get return message from server
+	/* Get return message from server */
 	memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer again for reuse
 	charsRead = recv(socketFD, buffer, sizeof(buffer) - 1, 0); // Read data from the socket, leaving \0 at end
+	
+	/* Error reading message from server */
 	if (charsRead < 0) error("CLIENT: ERROR reading from socket");
+
+	/* Print message from server */
 	printf("CLIENT: I received this from the server: \"%s\"\n", buffer);
 
-	close(socketFD); // Close the socket
+	close(socketFD);
+
 	return 0;
 }
